cf0e73e44852fc46b15a247d9e99b3d5de4fcd73
Dylan Collins <dcollins5590@students.mc3.edu>
2020-08-05 20:34:29 -0400

    finished 3 of the 8 files

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,28 @@
+
 def word_count(s):
-    # Your code here
-
-
+    cache = {}
+    array_string = s.split()
+    my_arr_needed = [""] * len(array_string)
+    if len(array_string) == 1:
+        array_string = [char for char in array_string[0]]
+    for i, ele in enumerate(array_string):
+        for letter in ele:
+            if letter.isalnum() or letter == "'":
+                my_arr_needed[i] += letter
+    if s is "":
+        return {}
+    else: 
+        for ele in my_arr_needed:
+            if ele == "":
+                return {}
+            if ele.lower() not in cache:
+                cache[ele.lower()] = 1
+            else:
+                cache[ele.lower()] = cache[ele.lower()] + 1
+        return cache
 
 if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
+    print(word_count("Hello     hello"))
+    print(word_count(":;,.-+=/\\|[]{}()*^&"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
